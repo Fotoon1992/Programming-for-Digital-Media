a608c685ac4400832f10b102367a35775c726593
Fotoon1992 <54630288+Fotoon1992@users.noreply.github.com>
2019-09-17 23:12:52 -0400

    Add files via upload

---
+++ Double,Double.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+def double(sequence):
+    result = []
+    for element in sequence:
+        result = result + [element * 2]
+        return result
+
+
+# In[ ]:
+
+
+
+
