96e5d13e95ae65ae9da01c9bd51256234b660dff
Moe <moe@Moes-MBP.attlocal.net>
2019-09-18 21:43:38 -0400

    class exercise

---
+++ madlibs.py
@@ -0,0 +1,23 @@
+import random
+#%%
+verbs = ["a", "b", "c"]
+print(verbs)
+
+#%%
+verbs = ["مرحبا"]
+print(verbs)
+
+#%%
+verbs = ["Hello"]
+print(verbs)
+
+#%%
+verbs = ["2019"]
+print(5+5)
+
+
+#%%
+verbs = ["runs", "jumps", "plays"]
+print("The boy " + random.choice(verbs) + " all day long")
+
+#%%
