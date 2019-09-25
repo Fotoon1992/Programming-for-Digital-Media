cfe7e8e3345d87b7d718104c92b4454e385d3d1c
fotoon1992 <fotoonhelmi@knights.ucf.edu>
2019-09-24 20:42:02 -0400

    tax

---
+++ tax.py
@@ -0,0 +1,7 @@
+def tax(subtotal):
+    return subtotal * 0.08875
+    tax(1500)
+    1500 + tax(1500)
+    200 + 1500 + tax(1500)
+    sum = 1500
+    sum = sum + sum * 0.08875 # Add the tax amount
