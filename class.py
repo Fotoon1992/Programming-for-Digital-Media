34eef8b1805efe40c5afc48562e53c2f0a04f7d8
fotoon1992 <fotoonhelmi@knights.ucf.edu>
2019-09-27 18:01:15 -0400

    multi values

---
+++ class.py
@@ -0,0 +1,23 @@
+#%%
+virbs = [1, 2]
+print("1 + 2")
+
+#%%
+def addNumbers(value1, value2, value3):
+    return value1
+
+#%%
+def addVal(a, b):
+    c = a + b
+    return c
+#%%
+def addnumbers(a, b, c):
+    return a + b + c
+
+#%%
+addval(3, 4)
+
+#%%
+addVal(3, 4)
+
+#%%
