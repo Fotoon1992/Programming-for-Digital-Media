777e10abed81619aadd707072e4babc5d81c1ae8
fotoon1992 <fotoonhelmi@knights.ucf.edu>
2019-09-27 17:58:56 -0400

    1

---
+++ Exercise 6-1.py
@@ -0,0 +1,14 @@
+#%%
+def half(sequense):
+    result = []
+    for element in sequense:
+        result = result + [element / 2]
+        return result
+
+#%%
+volume = range(100)
+result = []
+for element in volume:
+    result = result + [element / 2]
+
+#%%
