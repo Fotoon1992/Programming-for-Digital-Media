d2af6ba762c2238e3c7af2b53dd5ca7595aa4bc6
fotoon1992 <fotoonhelmi@knights.ucf.edu>
2019-09-27 17:59:59 -0400

    5

---
+++ Exercise 6-5.py
@@ -0,0 +1,8 @@
+#%%
+def ten_times_each(val1, val2, val3):
+    result = []
+    for element in val1, val2, val3:
+        result = [str(val1) * 10, str(val2) * 10, str(val3) * 10]
+        return result
+
+#%%
