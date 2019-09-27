db87ac305ef626732fdb85373045086d3ff07e85
fotoon1992 <fotoonhelmi@knights.ucf.edu>
2019-09-27 18:02:53 -0400

    book

--- function have scope.py
+++ function have scope.py
@@ -1,18 +1,23 @@
+#%%
 def set_a():
     a = 10
     print('The value of a:', a)
-    set_a()
-    a = 20
-    print('Initially, the value os a:', a)
-    set_a()
-    print('Initially, the value of a:', a)
+#%%
+ set_a()
+#%%
+a = 20
+print('Initially, the value of a:', a)
+set_a()
+print('Finally, the value of a:', a)
+#%%
 def scoped(first, second):
-    first = 10
-    second = 11
-    third = 12
     third = second + second - first
     return third
+
+#%%
+first = 10
+second = 11
+third = 12
 scoped(2, 4)
-first
-second
-third
+
+#%%
