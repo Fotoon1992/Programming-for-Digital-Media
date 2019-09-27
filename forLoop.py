a76902b05482b4b265fb561e9e8938ef9630758c
fotoon1992 <fotoonhelmi@knights.ucf.edu>
2019-09-27 18:00:37 -0400

    looping

---
+++ forLoop.py
@@ -0,0 +1,20 @@
+#%%
+sum = 0
+secondSum = 0
+for x in range(100):
+    sum = sum + x
+    secondSum = secondSum + x * x
+#%%
+def double(input):
+    for x in input:
+        print(x*2)
+
+#%%
+def exponent(x, y):
+    z = x
+    for n in range(1, y):
+        z = z * x
+        return z
+
+#%%
+exponent(5, 3)
