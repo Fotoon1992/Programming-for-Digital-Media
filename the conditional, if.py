92b42aa4c40660418d0b2f6ca004dc5a6ee4ef98
fotoon1992 <fotoonhelmi@knights.ucf.edu>
2019-09-27 18:01:42 -0400

    if book

---
+++ the conditional, if.py
@@ -0,0 +1,19 @@
+#%%
+def secret(word):
+    if word == 'please':
+        return True
+    else:
+        return False
+
+#%%
+def yesno(word):
+    if word == 'yes':
+        return True
+    if word == 'no':
+        return False
+
+#%%
+def secret(word):
+    return (word == 'please')
+
+#%%
