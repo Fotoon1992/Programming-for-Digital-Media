0f8ecc47ebd2f4c60fc44541679ce7bf318fcd86
fotoon1992 <fotoonhelmi@knights.ucf.edu>
2019-10-02 11:12:00 -0400

    organzation

---
+++ ch_6/Exercise 6-3.py
@@ -0,0 +1,16 @@
+#%%
+def has_no_elements(nothing):
+    if len(1) == 0:
+        return True
+    else:
+        return False
+#%%
+def has_no_elements():
+    return (len == 0)
+
+#%%
+def has_no_elements():
+    if len == 0:
+        return True
+
+#%%
---
+++ ch_6/Exercise 6-6.py
@@ -0,0 +1,12 @@
+#%%
+def positive(input):
+    for x in input:
+        print(x >= 0)       
+def pluses(input):
+    return sum (x > 0)
+
+      
+#%%
+
+def pluses(input):
+    sum = sum + x
