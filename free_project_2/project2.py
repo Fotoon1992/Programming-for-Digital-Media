e6cb78600ca1876f592402a9ce883dea234cc216
fotoon1992 <fotoonhelmi@knights.ucf.edu>
2019-11-04 13:58:05 -0500

    free_project_2

--- free_project_2/project2.py
+++ free_project_2/project2.py
@@ -4,7 +4,7 @@ img
 # %%
 #[Free Project 11-1] Image Manipulation as You Like It:
 from PIL import Image 
-img = Image.open('images.png')
+img = Image.open('free_project_2/images.png')
 def pixelize(img):
     new_size = (20, 20)
     connect = Image.BILINEAR
@@ -12,5 +12,5 @@ def pixelize(img):
     result = imgpix.resize(img.size, Image.NEAREST)
     return result
 pixelize(img)
-
+pixelize(img).save('free_project_2/img_pixelize.png')
 #%%
